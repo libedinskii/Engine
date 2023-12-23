63dbd212b1bcad6c37f70b2207d32e09e9ab505d
libedinskii <124511733+libedinskii@users.noreply.github.com>
2023-12-23 19:33:13 +0900

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Пример композиции
+        Car car = new Car();
+        car.startCar();
+
+        // Пример агрегации
+        Bicycle bicycle = new Bicycle();
+        bicycle.ride();
+    }
+}
